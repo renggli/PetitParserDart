058bb204b744d46a1a366eb51779d479e0ef2eec
Lukas Renggli <renggli@gmail.com>
2012-06-24 14:10:39 +0200

    - added code to read dart files from the filesystem

--- grammar/dart/dart.dart
+++ grammar/dart/dart.dart
@@ -2,11 +2,26 @@
 
 #library('dart');
 
+#import('dart:io');
 #import('../../lib/petitparser.dart');
 
 #source('dart_grammar.dart');
 #source('dart_parser.dart');
 
 void main() {
-  new DartParser();
+  Parser parser = new DartParser();
+  Map<String, String> sources = new Map();
+
+  Directory directory = new Directory('/Applications/Dart/dart-sdk/lib/');
+  DirectoryLister lister = directory.list(true);
+  lister.onFile = function(String filename) {
+    if (filename.endsWith('.dart')) {
+      sources[filename] = new File(filename).readAsTextSync(Encoding.UTF_8);
+    }
+  };
+  lister.onDone = function(completed) {
+    sources.forEach((key, value) {
+      print('$key: ${value.length}');
+    });
+  };
 }
